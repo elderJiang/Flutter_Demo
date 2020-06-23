fd95b671de591fd69a1f53507445eafc63d0708b
2623051778@qq.com <Chenyiqiang123>
2020-06-23 16:58:49 +0800

    练习

--- lib/pages/me_page.dart
+++ lib/pages/me_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutterappmmmm/pages/shop_page.dart';
 
 class MePage extends StatefulWidget {
   @override
@@ -8,7 +10,253 @@ class MePage extends StatefulWidget {
 class _MePageState extends State<MePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: Colors.grey[200],
+      child: ListView(
+        children: <Widget>[
+          HeadCard(),
+          MiddleWidget(),
+          DongtaiWiget(),
+          TowRowWidget(),
+        ],
+      ),
+    );
   }
 }
 
+//头部Header
+Widget HeadCard() {
+  return Container(
+    height: 300,
+    margin: EdgeInsets.only(left: 20, top: 20, right: 20),
+    child: Column(
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            ButtomWiget('设置', Icons.settings, true),
+            ButtomWiget('消息', Icons.message, true),
+          ],
+        ),
+        SizedBox(
+          height: 30,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            ButtomWiget(null, Icons.phone, false),
+            ButtomWiget(null, Icons.whatshot, false),
+            ButtomWiget(null, Icons.add_comment, false),
+            ButtomWiget(null, Icons.web, false),
+          ],
+        ),
+        Container(
+          width: 120,
+          height: 40,
+          margin: EdgeInsets.only(top: 30),
+          child: FlatButton(
+            onPressed: () {},
+            child: Text(
+              '登录/注册',
+              style: TextStyle(color: Colors.white),
+            ),
+            color: Colors.red,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+            ),
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.only(top: 30),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              ImageButton('动态', 'images/dongtai.png'),
+              ImageButton('跟帖', 'images/xiaoxi.png'),
+              ImageButton('收藏', 'images/shoucang.png'),
+              ImageButton('历史', 'images/lishi.png'),
+            ],
+          ),
+        )
+      ],
+    ),
+  );
+}
+
+Widget ButtomWiget(title, icon, bool havetitle) {
+  bool _haveTitle;
+  if (havetitle) {
+    return Container(
+      width: 80,
+      height: 40,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20.0),
+        border: Border.all(color: Colors.red, width: 1),
+      ),
+      child: FlatButton(
+        onPressed: () {},
+        child: Row(
+          children: <Widget>[
+            Icon(
+              icon,
+              size: 15,
+            ),
+            Text(title),
+          ],
+        ),
+      ),
+    );
+  } else {
+    return Container(
+      width: 50,
+      height: 50,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(25),
+        border: Border.all(color: Colors.red, width: 1),
+      ),
+      child: FlatButton(
+        onPressed: () {},
+        child: Container(
+          alignment: Alignment.center,
+          child: Icon(icon,size: 20,),
+        ),
+      ),
+    );
+  }
+}
+
+Widget ImageButton(title, imagestr) {
+  return Container(
+//    color: Colors.orange,
+    width: 50,
+    height: 60,
+    child: Column(
+      children: <Widget>[
+        Container(
+          child: Image.asset(imagestr),
+        ),
+        Container(
+          margin: EdgeInsets.only(top: 5),
+          child: Text(
+            title,
+            style: TextStyle(fontSize: 12),
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+//中间
+Widget MiddleWidget() {
+  return Container(
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.all(Radius.circular(8)),
+    ),
+    margin: EdgeInsets.only(
+      left: 20,
+      right: 20,
+      bottom: 10,
+    ),
+    height: 100,
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: <Widget>[
+        ImageButton('我的关注', 'images/dongtai.png'),
+        ImageButton('任务中心', 'images/xiaoxi.png'),
+        ImageButton('金币商城', 'images/shoucang.png'),
+        ImageButton('会员中心', 'images/lishi.png'),
+      ],
+    ),
+  );
+}
+
+//发布动态
+Widget DongtaiWiget() {
+  return Container(
+    height: 60,
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.all(Radius.circular(8)),
+    ),
+    margin: EdgeInsets.only(left: 20,right: 20),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.only(left: 20),
+          child: Text('发布我的动态'),
+        ),
+        Container(
+//          margin: EdgeInsets.only(right: 20),
+          child: FlatButton(
+            onPressed: () {},
+            child: Container(
+              width: 80,
+              height: 40,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+              ),
+              child: Text(
+                '发布',
+                style: TextStyle(color: Colors.white, fontSize: 14),
+              ),
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+Widget TowRowWidget() {
+  return Container(
+    margin: EdgeInsets.only(top: 10,left: 20,right: 20),
+    height: 110,
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.all(Radius.circular(8)),
+    ),
+    child: Column(
+      children: <Widget>[
+        CommonItem('深色模式', '去系统设置'),
+        Divider(color: Colors.grey[300],indent: 20,endIndent: 20,height: 1,),
+        CommonItem('意见反馈', ''),
+      ],
+    ),
+  );
+}
+
+Widget CommonItem(title, subtitle) {
+  String subtitleStr = subtitle.toString();
+  return Container(
+    height: 50,
+    child: Row(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.only(left: 20),
+          child: Text(title),
+        ),
+        Expanded(
+          flex: 5,
+          child: Container(
+            alignment: Alignment.centerRight,
+            child: Text(subtitleStr.length > 0 ? subtitleStr : '',style: TextStyle(color: Colors.grey[400]),),
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Container(
+            width: 50,
+//            color: Colors.orange,
+//            alignment: Alignment.r,
+            child: Icon(Icons.arrow_forward_ios,color: Colors.grey[500],size: 20,),
+          ),
+        ),
+      ],
+    ),
+  );
+}
--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class ShopPage extends StatefulWidget {
   @override
@@ -6,8 +7,155 @@ class ShopPage extends StatefulWidget {
 }
 
 class _ShopPageState extends State<ShopPage> {
+
+  List <Widget> _wigets = [getCommonItem(0),getImageItem(0)];
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView.builder(
+      itemCount: 1,
+      itemBuilder: (context, index) {
+        return getImageItem(index);
+
+      },
+    );
+  }
+}
+
+Widget getCommonItem(index) {
+  return FlatButton(
+    child: Container(
+      child: Row(
+        children: <Widget>[
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Container(
+                width: 200,
+                height: 60,
+//                color: Colors.orange,
+                padding: EdgeInsets.only(top: 10),
+                child: Text(
+                  '印军方授权一线部队使用武器?媒体:印度或付出代价',
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                ),
+              ),
+              Container(
+                width: 200,
+                margin: EdgeInsets.only(top: 10),
+//                color: Colors.amber,
+                alignment: Alignment.topLeft,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Container(
+//                      color: Colors.orange,
+                      child: Text(
+                        '热门',
+                        style: TextStyle(color: Colors.red, fontSize: 14),
+                      ),
+                      alignment: Alignment.topLeft,
+                    ),
+                    Text(
+                      '环球时报',
+                      style: TextStyle(color: Colors.black12),
+                    ),
+                    TagWiget(),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Expanded(
+            flex: 1,
+            child: AspectRatio(
+              aspectRatio: 3 / 2,
+              child: Container(
+                margin: EdgeInsets.only(left: 20),
+                foregroundDecoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(
+                        'http://cms-bucket.ws.126.net/2020/0623/69c7d26fp00qccouz00msc000s600e3c.png'),
+                  ),
+                  borderRadius: BorderRadius.all(Radius.circular(6.0)),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    ),
+    onPressed: () {},
+  );
+}
+
+Widget getImageItem(index) {
+  return FlatButton(
+      onPressed: () {},
+      child: Container(
+        color: Colors.orange,
+        margin: EdgeInsets.all(20),
+        height: 200,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Expanded(
+              flex: 1,
+              child: Text('鞠婧祎为显脸小戴大号口罩？网友吐槽：做作！'),
+            ),
+            Expanded(
+              flex: 2,
+              child: imageWidget(),
+            ),
+            Expanded(
+              flex: 1,
+              child: Text('读史 259跟帖'),
+            ),
+          ],
+        ),
+      ));
+}
+
+class imageWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 80,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+
+          Image.network('http://cms-bucket.ws.126.net/2020/0623/13b066f7p00qccet800f6c000s600e3c.png'),
+          Image.network('http://cms-bucket.ws.126.net/2020/0622/140538edp00qcbbv3002jc0009c005uc.png'),
+          Image.network('http://cms-bucket.ws.126.net/2020/0623/69e81f45p00qccpkx003oc0009c005uc.png'),
+
+        ],
+      ),
+    );
+  }
+}
+
+class TagWiget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 24,
+      width: 60,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.red, width: 0.5),
+        color: Colors.white,
+        borderRadius: BorderRadius.all(Radius.circular(12)),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+//          Icon(Icons.hot_tub),
+          Text(
+            '2.5万跟帖',
+            style: TextStyle(color: Colors.red, fontSize: 9),
+          ),
+        ],
+      ),
+    );
   }
 }
